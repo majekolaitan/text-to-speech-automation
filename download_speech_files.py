39ae910b8230e231eb4dae6c4016d1d357c7c1c9
Olaitan Majekodunmi <majekolaitan@gmail.com>
2024-05-25 06:57:46 +0100

    added download speech script

--- download_speech_files.py
+++ download_speech_files.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
+import requests
 
 driver = webdriver.Chrome()
 
@@ -10,6 +12,16 @@ submit_button_id = 'login_button'
 
 your_username = 'enter_your_email'
 your_password = 'enter_your_password'
+
+# Function to download file
+def download_file(url, folder):
+    local_filename = os.path.join(folder, url.split('/')[-1])
+    with requests.get(url, stream=True) as r:
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+    return local_filename
 
 # Open the webpage
 driver.get("https://app.blasteronline.com/user/login")
@@ -22,14 +34,22 @@ driver.find_element(By.ID, password_id).send_keys(your_password)
 time.sleep(20)
 
 # Find all <a> elements
-link_elements = driver.find_elements_by_xpath("//td/a")
+elements = driver.find_elements(By.CSS_SELECTOR, "#blastered_datatable > tbody > tr > td:nth-child(7) > a")
 
-# Extract href attributes from each <a> element
-href_values = [link.get_attribute("href") for link in link_elements]
+# Create a folder to save the downloaded files if it doesn't exist
+download_folder = 'downloads'
+if not os.path.exists(download_folder):
+    os.makedirs(download_folder)
 
-# Print the extracted href values
-for href in href_values:
-    print(href)
+# Extract href attributes from each element
+hrefs = [element.get_attribute("href") for element in elements]
 
-# Close the browser
+# Download each file
+for element in elements:
+    href = element.get_attribute("href")
+    if href.endswith('.mp3'):
+        filename = download_file(href, download_folder)
+        print("Downloaded:", filename)
+
+# Close the WebDriver
 driver.quit()
